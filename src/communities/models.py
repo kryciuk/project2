547ba452a4d9fff1ab0a5d0a21fcaddcb7f87da8
Klaudia Ryciuk <rinrearoa@gmail.com>
2024-08-15 17:32:36 +0200

    feat: add __str__ for Buliding for better display for user

--- src/communities/models.py
+++ src/communities/models.py
@@ -9,10 +9,16 @@ class Building(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(null=True, blank=True)
     city = models.CharField(choices=CHOICES_CITIES)
-    postal_code = models.CharField(max_length=7, null=True, blank=True, validators=[RegexValidator(regex='^[0-9]{2}-[0-9]{3}$')])
+    postal_code = models.CharField(
+        max_length=7, null=True, blank=True, validators=[RegexValidator(regex="^[0-9]{2}-[0-9]{3}$")]
+    )
     street = models.CharField(max_length=30, null=True, blank=True)
     street_number = models.IntegerField(null=True, blank=True)
+
     # manager_id = models.ForeignKey()
+
+    def __str__(self):
+        return f"{self.name} ID:{self.pk}"
 
     def get_absolute_url(self):
         return reverse("building-create")
