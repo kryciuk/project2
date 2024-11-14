406438cd7b38ddbcb2aabc69b77fd72fc5282c1a
kryciuk <rinrearoa@gmail.com>
2024-11-14 17:10:58 +0100

    fix: update CustomInvitation to send invitations to Property Managers without specifying buildings

--- src/users/models.py
+++ src/users/models.py
@@ -15,5 +15,5 @@ class CustomInvitation(Invitation):
         GROUP__RESIDENT = "Resident"
         GROUP__ADMINISTRATOR = "Administrator"
 
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, null=True, blank=True)
     group = models.CharField(choices=GroupChoices.choices)
