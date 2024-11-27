321593352918f1397f639a889d4ba1f8411a488a
kryciuk <rinrearoa@gmail.com>
2024-11-27 19:15:33 +0100

    feat: sort issues by property managers

--- src/issues/filters.py
+++ src/issues/filters.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 from communities.models import Building
 from issues.models import Issue
+from users.models import CustomUser
 
 
 class IssuePropertyManagerFilter(django_filters.FilterSet):
@@ -14,22 +15,12 @@ class IssuePropertyManagerFilter(django_filters.FilterSet):
         field_name="severity", choices=Issue.IssueSeverityChoices.choices, label=_("Severity")
     )
     building = django_filters.ModelChoiceFilter(queryset=Building.objects.all(), label=_("Building"))
+    manager = django_filters.ModelChoiceFilter(
+        field_name="building__manager",
+        queryset=CustomUser.objects.filter(groups__in=[1, 2]),
+        label=_("Property Manager"),
+    )
 
     class Meta:
         model = Issue
         fields = ["title", "status", "severity", "building"]
-
-
-# class IssueClosedFilter(django_filters.FilterSet):
-#     title = django_filters.CharFilter(field_name="title", lookup_expr="iexact", label=_("Title"))
-#     status = django_filters.ChoiceFilter(
-#         field_name="status", choices=Issue.IssueStatusChoices.choices, label=_("Status")
-#     )
-#     severity = django_filters.ChoiceFilter(
-#         field_name="severity", choices=Issue.IssueSeverityChoices.choices, label=_("Severity")
-#     )
-#     building = django_filters.ModelChoiceFilter(queryset=Building.objects.all(), label=_("Building"))
-#
-#     class Meta:
-#         model = Issue
-#         fields = ["title", "status", "severity", "building"]
