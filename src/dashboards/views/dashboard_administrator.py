c434c45654d9082b597882c0fbe2cde228c8e684
kryciuk <rinrearoa@gmail.com>
2024-12-02 20:59:05 +0100

    refactor: clean code

--- src/dashboards/views/dashboard_administrator.py
+++ src/dashboards/views/dashboard_administrator.py
@@ -3,8 +3,4 @@ from django.views.generic import TemplateView
 
 class AdministratorDashboardView(TemplateView):
     template_name = "dashboards/dashboard_administrator.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Project2 - Administrator"
-        return context
+    extra_context = {"title": "Project2 - Administrator"}
