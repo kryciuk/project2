f6b3df5232b198b8320dd83e2f1054aadcd6377b
kryciuk <rinrearoa@gmail.com>
2024-12-05 20:51:29 +0100

    feat: add permissions for views

--- src/landing/apps.py
+++ src/landing/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
+from core.utils import _get_perms_for_models
+from issues.models import Issue
+
 
 class LandingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -10,16 +13,38 @@ class LandingConfig(AppConfig):
         post_migrate.connect(self.populate_models, sender=self)
 
     def populate_models(self, sender, **kwargs):
-        from django.contrib.auth.models import Group
+        from django.contrib.auth.models import Group, Permission
+        from django.contrib.contenttypes.models import ContentType
+
+        from communities.models import Building
+
+        # all model permissions
+
+        models_to_fetch_administrator = [Building, Issue]
+        models_to_fetch_property_manager = [Issue]
+
+        # single permissions
+
+        content_type_profile = ContentType.objects.get_for_model(Building)
+
+        permission_update_building = Permission.objects.get(
+            codename="change_building", content_type=content_type_profile
+        )
+        permission_view_building = Permission.objects.get(codename="view_building", content_type=content_type_profile)
+        permission_add_issue = Permission.objects.get(codename="add_issue", content_type=content_type_profile)
 
         # administrator
 
         administrator, _ = Group.objects.get_or_create(name="Administrator")
+        administrator.permissions.set(_get_perms_for_models(models_to_fetch_administrator))
 
         # property_manager
 
         property_manager, _ = Group.objects.get_or_create(name="Property Manager")
+        administrator.permissions.set(_get_perms_for_models(models_to_fetch_property_manager))
+        property_manager.permissions.add([permission_update_building, permission_view_building])
 
         # resident
 
         resident, _ = Group.objects.get_or_create(name="Resident")
+        property_manager.permissions.add([permission_add_issue])
