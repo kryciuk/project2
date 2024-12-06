5480daa2f1d9b292e2a3abb47b785648728e2b40
kryciuk <rinrearoa@gmail.com>
2024-12-06 17:45:18 +0100

    fix: fix permissions settings

--- src/landing/apps.py
+++ src/landing/apps.py
@@ -17,23 +17,26 @@ class LandingConfig(AppConfig):
 
         from communities.models import Building
         from issues.models import Comment, Issue
+        from users.models import CustomInvitation
 
         # all model permissions
 
-        models_to_fetch_administrator = [Building, Issue, Comment]
-        models_to_fetch_property_manager = [Issue]
+        models_to_fetch_administrator = [Building, Issue, Comment, CustomInvitation]
+        models_to_fetch_property_manager = [Issue, CustomInvitation]
 
         # single permissions
 
-        content_type_profile = ContentType.objects.get_for_model(Building)
+        content_type_building = ContentType.objects.get_for_model(Building)
+        content_type_issue = ContentType.objects.get_for_model(Issue)
+        content_type_comment = ContentType.objects.get_for_model(Comment)
 
         permission_update_building = Permission.objects.get(
-            codename="change_building", content_type=content_type_profile
+            codename="change_building", content_type=content_type_building
         )
-        permission_view_building = Permission.objects.get(codename="view_building", content_type=content_type_profile)
-        permission_add_issue = Permission.objects.get(codename="add_issue", content_type=content_type_profile)
-        permission_view_issue = Permission.objects.get(codename="view_issue", content_type=content_type_profile)
-        permission_add_comment = Permission.objects.get(codename="add_comment", content_type=content_type_profile)
+        permission_view_building = Permission.objects.get(codename="view_building", content_type=content_type_building)
+        permission_add_issue = Permission.objects.get(codename="add_issue", content_type=content_type_issue)
+        permission_view_issue = Permission.objects.get(codename="view_issue", content_type=content_type_issue)
+        permission_add_comment = Permission.objects.get(codename="add_comment", content_type=content_type_comment)
 
         # administrator
 
@@ -44,11 +47,13 @@ class LandingConfig(AppConfig):
 
         property_manager, _ = Group.objects.get_or_create(name="Property Manager")
         administrator.permissions.set(_get_perms_for_models(models_to_fetch_property_manager))
-        property_manager.permissions.add(
-            [permission_update_building, permission_view_building, permission_add_comment]
-        )
+        property_manager.permissions.add(permission_update_building)
+        property_manager.permissions.add(permission_view_building)
+        property_manager.permissions.add(permission_add_comment)
 
         # resident
 
         resident, _ = Group.objects.get_or_create(name="Resident")
-        property_manager.permissions.add([permission_add_issue, permission_view_issue, permission_add_comment])
+        resident.permissions.add(permission_add_issue)
+        resident.permissions.add(permission_view_issue)
+        resident.permissions.add(permission_add_comment)
